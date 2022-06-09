38e08fdf37f3f5e65136da80bfa67f27b858905d
Delaney Fogarty <delaney.fk@gmail.com>
2022-06-09 15:40:00 -0700

    tests written

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -3,13 +3,41 @@ const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
 
+const herbs = require('../data/herb-data');
+
 describe('backend-express-template routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
-  it('example test - delete me!', () => {
-    expect(1).toEqual(1);
+
+  it('/herbs should return list of all herbs', async () => {
+    const res = await request(app).get('/herbs');
+    const expected = herbs.map((herb) => {
+      return {
+        id: herb.id,
+        name: herb.name,
+        scientific_name: herb.scientific_name,
+        properties: herb.properties,
+        direct_sunlight: herb.direct_sunlight,
+        uses: herb.uses,
+      };
+    });
+    expect(res.body).toEqual(expected);
   });
+
+  it('/herbs/:id should return one herbs details', async () => {
+    const res = await request(app).get('/herbs/1');
+    const calendula = {
+      id: '1',
+      name: 'Calendula',
+      scientific_name: 'Calendula officinalis',
+      healing_properties: 'antiseptic, detoxifying, antispasmodic',
+      direct_sunlight: true,
+      uses: 'tea for internal and external ailments',
+    };
+    expect(res.body).toEqual(calendula);
+  });
+
   afterAll(() => {
     pool.end();
   });
